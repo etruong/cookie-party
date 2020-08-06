0cc5bf67ccf785b95f5afaae855ee799c0de8051
Elisa <elisat15@uw.edu>
2020-08-05 18:23:29 -0700

    empty

--- cookie-party.js
+++ cookie-party.js
@@ -1,3 +1,23 @@
+// Instructions:
+// 
+// 1. When the window/browser loads...
+//       - generate the dropdown menu with options
+//       - add an on change event listener for the dropdown menu
+//       - add an on click event listener for the cookie jar
+// 2. Create a function called generateCookie that creates the selected
+//    cookie DOM element and attaches it to the plate, make sure the cookie
+//    has the following traits: 
+//       - is an img tag
+//       - alt attribute containing the type of cookie it is
+//       - contains the class cookie
+// 3. Create a function called monsterEat that contains a parameter called cookie
+//    that is the target cookie that cookie monster will eat that does the
+//    following:
+//       - removes the cookie from the plate
+//       - calls calculateHunger function (found in the helper-function.js) passing
+//         in the cookie that is to be removed this should adjust the health bar depending
+//         on the cookie passed in
+
 // Types of cookies and how much they fill up cookie monster
 const cookies ={
     burnt: -20,
@@ -9,33 +29,3 @@ const cookies ={
 }
 
 let selectedCookie = 'burnt'; // Cookie that is selected
-
-// Code that is run when html/window page is loaded
-window.addEventListener('load', () => {
-
-    // DOM elements/Variables needed
-    const dropdownMenu = qs('select');
-    const cookieJar = qs('#jar-container img');
-    const cookieTypes = Object.keys(cookies);
-
-    // TODO Step 2.1: Generate dropdown options below
-
-    // TODO Step 2.2: Add an on change event listener for the dropdown menu
-
-    // TODO Step 3.1: Add an on click event listener for the cookie jar
-    
-});
-
-// TODO Step 3.2: Generates a cookie with selected type of cookie
-// Hint: this is where the code for Step 4.2 should be placed also!
-function generateCookie() {
-    const cookieContainer = qs('#cookie-container');
-
-}
-
-// TODO Step 4.1: On click callback function where monster eats given cookie 
-function monsterEat(cookie) {
-    const cookieContainer = qs('#cookie-container');
-    const fullBar = qs('.full-bar');
-
-}
